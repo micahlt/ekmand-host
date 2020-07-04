498c3c22b47f0ec11303f92ef884f8dd53a472c5
micahlt <micahincorporated@gmail.com>
2020-07-04 09:39:32 -0500

    Minor server changes

--- server.js
+++ server.js
@@ -11,4 +11,12 @@ app.get('/favicon.ico' || '/favicon.png', (req, res) => {
   res.sendFile(__dirname + '/assets/favicon.png');
 });
 
+app.get('/api', (req, res) => {
+  res.send("Ekmand Host API, running live on port " + (process.env.PORT || 8000));
+})
+
+app.put('/api/verify', (req, res) => {
+  res.send(req);
+})
+
 app.listen((process.env.PORT || 8000), () => console.log(`Ekmand Host listening on port`, (process.env.PORT || 8000)));
